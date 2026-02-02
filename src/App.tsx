5295d31d38d3d1bd533862d6140a8903184232c9
barbara.lemes <barbaraayllon@gmail.com>
2026-02-01 23:58:04 -0400

    feat(auth): verifica autenticação global ao iniciar aplicação via App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+import { router } from "./routes/index";
+import { authFacade } from "./modules/auth/services/auth.facade";
+
 export default function App() {
-  return (
-    <>
-    </>
-  )
+  useEffect(() => {
+    authFacade.checkAuth();
+  }, []);
+
+  return <RouterProvider router={router} />;
 }
